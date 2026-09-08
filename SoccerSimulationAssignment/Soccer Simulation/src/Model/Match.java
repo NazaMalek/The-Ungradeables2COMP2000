6ceae47b8e52d50b6d3e92a3409f7d05c0395fa1
WilliamBrown5 <william.brown5@students.mq.edu.au>
2026-09-08 14:05:21 +1000

    outlined what I need to do for the match class

--- SoccerSimulationAssignment/Soccer Simulation/src/Model/Match.java
+++ SoccerSimulationAssignment/Soccer Simulation/src/Model/Match.java
@@ -1,1 +1,12 @@
 // WILLIAM IS DOING THIS
+/*
+use the display already made in the soccer pitch class, 
+the match logic, goal scoring and ref stuff is being done by other people 
+all this class will do will be to put players on the pitch in certain formations 
+i also have to handle the gui for everything while simulation is running a mini print log 
+execute events just be an area for stuff to happen like pull everything together 
+
+
+
+ */
+
