7eedc1b65dfb1637ab0f8f2ad6c8d31a51c887a8
ethereal-phanthom <debnatharpan67@icloud.com>
2026-09-12 14:00:06 +1000

    Delete SoccerSimulationAssignment/Soccer Simulation/src/Model/StaminaExhaustedException.java

--- SoccerSimulationAssignment/Soccer Simulation/src/Model/StaminaExhaustedException.java
+++
@@ -1,9 +0,0 @@
-/**
- * Checked exception thrown when an offensive player attempts a high-intensity action
- * without sufficient stamina reserves.
- */
-public class StaminaExhaustedException extends Exception {
-    public StaminaExhaustedException(String message) {
-        super(message);
-    }
-}
